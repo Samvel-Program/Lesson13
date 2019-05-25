e26a95edab4f130cc14fae00b52de1b1d57f966c
Samvel Akopyan <samvel.mgsu@mail.ru>
2019-05-25 19:22:06 +0300

    commit

---
+++ src/Program.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Created by OUT-Akopyan-SR on 24.05.2019.
+ */
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Введите текст с пробелами");
+        Scanner in = new Scanner(System.in);
+        String s1 = in.nextLine();
+        String s2 = in.nextLine();
+
+
+        if (s1.length() > s2.length()) {
+            System.out.println(s1);
+        } else {
+            System.out.println(s2);
+        }
+
+
+    }
+}
